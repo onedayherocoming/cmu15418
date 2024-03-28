be04f8f53bde3f8836d3f43e3be3de78d3d6b716
max.qin <max.qin@cad-timely-involve-cx3-max-qin-master-0.cad-timely-involve-cx3-max-qin.wsgo-max-qin.svc.cluster.local>
2024-03-28 08:18:15 +0000

    complete prog4

--- asst1/prog4_sqrt/data.cpp
+++ asst1/prog4_sqrt/data.cpp
@@ -1,8 +1,9 @@
 #include <algorithm>
 
 // Generate random data
-void initRandom(float *values, int N) {
-    for (int i=0; i<N; i++)
+void initRandom(float *values, int N)
+{
+    for (int i = 0; i < N; i++)
     {
         // random input values
         values[i] = .001f + 2.998f * static_cast<float>(rand()) / RAND_MAX;
@@ -10,8 +11,9 @@ void initRandom(float *values, int N) {
 }
 
 // Generate data that gives high relative speedup
-void initGood(float *values, int N) {
-    for (int i=0; i<N; i++)
+void initGood(float *values, int N)
+{
+    for (int i = 0; i < N; i++)
     {
         // Todo: Choose values
         values[i] = 1.0f;
@@ -19,11 +21,11 @@ void initGood(float *values, int N) {
 }
 
 // Generate data that gives low relative speedup
-void initBad(float *values, int N) {
-    for (int i=0; i<N; i++)
+void initBad(float *values, int N)
+{
+    for (int i = 0; i < N; i++)
     {
         // Todo: Choose values
-        values[i] = 1.0f;
+        values[i] = 2.0f;
     }
 }
-
